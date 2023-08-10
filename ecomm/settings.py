3071b6f317acabb1d50054d1d3180f50a6791c63
vishalkhandagale <vish.khandagale20302@gmail.com>
2023-08-10 19:50:53 +0530

    static files setting

--- ecomm/settings.py
+++ ecomm/settings.py
@@ -124,12 +124,14 @@ STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles' , 'staticfiles_build')
 
 STATICFILES_DIR = {
     os.path.join(BASE_DIR , "public/static")
 }
 
+STATICFILES_DIR = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 MEDIA_ROOT =  os.path.join(BASE_DIR, 'public/static') 
 MEDIA_URL = '/media/'
 
